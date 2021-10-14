c89e2ea54717c55403e71a64290945e4361bdfc5
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2021-10-14 22:17:01 +0200

    Flakify nix build.

--- nix/regenerate.sh
+++ nix/regenerate.sh
@@ -2,13 +2,25 @@
 
 set -euo pipefail
 
-nix="$(dirname "$0")/default.nix"
+cd $(dirname "$0")/..
+
+# Regenerate sha256map.nix
+nix-build ./nix -A sha256mapRegenerate
+./result
 
 # Regenerate stack-to-nix files in ./.stack-nix
-$(nix-build "$nix" -A stackNixRegenerate --no-out-link) "$@"
+rm -f nix/materialized/stack-nix/.stack-to-nix.cache #https://github.com/input-output-hk/haskell.nix/issues/57
+nix-build ./nix -A cardanoWalletHaskellProject.stack-nix.passthru.generateMaterialized
+./result nix/materialized/stack-nix/
 
 # Regenerate materialized haskell-build-tools in ./materialized
-$(nix-build "$nix" -A haskell-build-tools.regenerateMaterialized --no-out-link)/bin/regenerate-materialized-nix
+nix-build ./nix -A haskell-build-tools.regenerateMaterialized
+./result/bin/regenerate-materialized-nix
 
 # Regenerate materialized iohk-nix-utils in ./materialized
-$(nix-build "$nix" -A iohk-nix-utils.regenerateMaterialized --no-out-link)/bin/regenerate-materialized-nix
+nix-build ./nix -A iohk-nix-utils.regenerateMaterialized
+./result/bin/regenerate-materialized-nix
+
+# Regenerate the list of the project packages:
+nix eval -f nix/default.nix cardanoWalletLib.projectPackageList > nix/project-package-list.nix.new
+mv nix/project-package-list.nix.new nix/project-package-list.nix
